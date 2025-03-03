e28fff97dc1378c52f9bcb80247e82579d8c53dc
Roberto Ruiz <R.Ruiz@ext.aurubis.com>
2025-03-03 01:40:11 +0100

    chore: fix metadata

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
-export const GENERATE_SLUG_FROM_TITLE = true
-export const TRANSITION_API = true
+export const SITE_TITLE =
+  "Roberto Ruiz | Frontend Developer, Mentor & UX Researcher";
+export const SITE_DESCRIPTION = `I'm a UI Engineer who loves teaching other people about Frontend development. I'm currently doing a masters degree in Human-Computer Interaction in Germany.`;
+export const GENERATE_SLUG_FROM_TITLE = true;
+export const TRANSITION_API = true;
