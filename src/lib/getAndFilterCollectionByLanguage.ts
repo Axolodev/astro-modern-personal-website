22db4d8567de6e18eb48dc72ba5c666674939473
Roberto Ruiz <R.Ruiz@ext.aurubis.com>
2025-03-03 01:33:07 +0100

    feat: add function to filter projects by language

---
+++ src/lib/getAndFilterCollectionByLanguage.ts
@@ -0,0 +1,22 @@
+import { getCollection } from "astro:content";
+
+const defaultRegex = new RegExp(`^[^\\.]+\\.md$`);
+
+export async function getAndFilterCollectionByLanguage(
+  collectionName: Parameters<typeof getCollection>[0],
+  language: string = "default"
+) {
+  // Checks if the id of the current item ends with [title].[language].md
+  const regex = new RegExp(`\\.${language}\\.md$`);
+
+  if (language !== "default") {
+    return (await getCollection(collectionName)).filter((item) =>
+      regex.test(item.id)
+    );
+  }
+
+  // Gets all the item that end follow the structure [title].md
+  return (await getCollection(collectionName)).filter((item) =>
+    defaultRegex.test(item.id)
+  );
+}
